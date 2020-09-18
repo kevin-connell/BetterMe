cb4b266998517756a7c3e4ad8a0f666336d5d765
JoshTrev <joshj438t@gmail.com>
2020-09-18 11:34:34 -0400

    Think im doing this right?

--- recipe-card.js
+++ recipe-card.js
@@ -45,84 +45,6 @@ $(document).ready(function () {
         return Object.keys(newObj);
     }
 
-<<<<<<< HEAD
-    clickedFav = localStorage.getItem("clickedFavorite")
-
-    if (clickedFav !== "no" || !clickedFav) {
-
-        clickedFav = localStorage.getItem("clickedFavorite")
-
-        var queryURL = "https://api.edamam.com/search?q=" + clickedFav + "&app_id=a9f85e6a&app_key=77f6e24e315d52d60bcbb1e1b428e579&from=0&to=1"
-
-        console.log(queryURL)
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-            console.log(response)
-
-            var clicked = response.hits
-
-            showSelected(clicked)
-
-            localStorage.setItem("clickedFavorite" , "no")
-        })
-
-    } else {
-
-        // Getting Selected Recipe From Storage\
-
-        var selectedRecipe = JSON.parse(localStorage.getItem("selectedRecipe"));
-
-        showSelected(selectedRecipe)
-    };
-
-    function showSelected(x) {
-
-        // Variables for Selected Recipe
-        console.log(x);
-
-        var title = x[0].recipe.label;
-        var image = x[0].recipe.image;
-        var ingredients = x[0].recipe.ingredientLines;
-        var calories = x[0].recipe.calories;
-        var servings = x[0].recipe.yield;
-
-        var source = x[0].recipe.source;
-        var url = x[0].recipe.url;
-
-        $("#recipeImage").attr("src", image);
-        $("#recipeImage").attr("alt", title + "image");
-        $("#recipeTitle").text(title);
-
-        $("#recipeCalories").text("Calories: " + calories.toFixed(0));
-        $("#recipeServings").text("Makes " + servings + " servings.");
-
-        $("#recipeCaloriesPerServing").text("Calories per serving: " + (calories / servings).toFixed(0));
-
-        for (i = 0; i < ingredients.length; i++) {
-
-            var newIngredient = $("<li></li>");
-
-            $("#ingredients").append(newIngredient);
-
-            $(newIngredient).text(ingredients[i]);
-        }
-
-        $("#recipeLink").attr("href", url);
-        $("#recipeLink").text(source);
-
-    }
-
-    //     $("#recipeImage").attr("src" , image)
-    //     $("#recipeImage").attr("alt" , title + "image")
-    //     $("#recipeTitle").text(tabel)
-    //     $("#recipeLink").attr("href" , url)
-    //     $("#recipeLink").text(source)
-    // });
-=======
     // Getting Selected Recipe From Storage\
 
     var selectedRecipe = JSON.parse(localStorage.getItem("selectedRecipe"));
@@ -220,5 +142,4 @@ $(document).ready(function () {
 
         $("#workoutDescription").text(response.description);
     });
->>>>>>> 2057d400e7739851d96ad05a2fb6662014f5b0eb
 });
